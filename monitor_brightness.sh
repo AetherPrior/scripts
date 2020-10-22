bf0a03d7db5463d21481443d4d187918e52fa38e
Abhinav Sukumar Rao <39939017+AetherPrior@users.noreply.github.com>
2020-10-23 00:36:59 +0530

    Add scripts

---
+++ monitor_brightness.sh
@@ -0,0 +1,2 @@
+xrandr --output HDMI1 --brightness 0.5
+xrandr --output eDP1 --brightness 0.5
---
+++ tablet.sh
@@ -0,0 +1,6 @@
+xsetwacom --set "Wacom Bamboo One S Pen stylus" rotate half
+xsetwacom --set "Wacom Bamboo One S Pen eraser" rotate half
+xsetwacom --set "Wacom Bamboo One S Pen stylus" MapToOutput "HDMI1"
+xsetwacom --set "Wacom Bamboo One S Pen eraser" MapToOutput "HDMI1"
+xsetwacom --set "Wacom Bamboo One S Pen eraser" mode absolute
+xsetwacom --set "Wacom Bamboo One S Pen stylus" mode absolute
